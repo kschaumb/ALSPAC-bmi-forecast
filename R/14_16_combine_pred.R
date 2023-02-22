17afefdd3a3db3bb3fdb3cd982da3e6b0e7c9a49
kschaumb <katherine.schaumberg@gmail.com>
2023-02-22 08:41:49 -0600

    doc updates

--- R/14_16_combine_pred.R
+++ R/14_16_combine_pred.R
@@ -37,9 +37,14 @@ fasting_14_16_monthly <- ALSPAC_cleaned$Compensatory_Behaviors |>
 exercise_14_16 <- ALSPAC_cleaned$Driven_Exercise |>
   filter(assess_agemos < 200) |>
   group_by(id) |>
-  summarise(DEx_14_16  = case_when(sum(driven_exercise_2, na.rm =TRUE)>0 ~1,
-                                   sum(driven_exercise_2, na.rm = TRUE)==0 ~0)) |>
-  filter(!is.na(DEx_14_16))
+  summarise(exercise_14_16  = case_when(sum(driven_exercise, na.rm =TRUE)>0 ~1,
+                                   sum(driven_exercise, na.rm = TRUE)==0 ~0)) 
+
+reg_exercise_14_16 <- ALSPAC_cleaned$Driven_Exercise |> 
+  filter(assess_agemos < 200) |>
+  group_by(id) |>
+  summarise(reg_exercise_14_16  = case_when(sum(driven_exercise_2, na.rm =TRUE)>0 ~1,
+                                        sum(driven_exercise_2, na.rm = TRUE)==0 ~0)) 
 
 binge_14_16 <- ALSPAC_cleaned$Binge |>
   filter(assess_agemos < 200) |>
@@ -48,8 +53,34 @@ binge_14_16 <- ALSPAC_cleaned$Binge |>
             sum(binge_freq_d, na.rm = TRUE)==0 ~0)) |>
   filter(!is.na(binge_14_16))
 
-bmi_13_16 <- bmi_df_13_16 |>  group_by(id) |>
-  summarise(bmi_loss_99 = case_when(sum(lower_99_cutoff, na.rm =TRUE)>0 ~1
+
+
+df_16 <- left_join(bmi_pred_Mean, exercise_14_16)
+df_16 <- left_join(df_16, reg_exercise_14_16)
+df_16 <- left_join(df_16, fasting_14_16)
+df_16 <- left_join(df_16, fasting_14_16_monthly)
+df_16 <- left_join(df_16, purging_14_16)
+df_16 <- left_join(df_16, purging_14_16_monthly)
+
+
+
+df_16 <- df_16 |>
+  mutate(sum_compensatory_any  = rowSums(across(c(purging_14_16, fasting_14_16, exercise_14_16)))) |>
+  mutate(any_compensatory = case_when(sum_compensatory_any >0 ~1,
+                                      sum_compensatory_any ==0 ~0)) %>% 
+  mutate(sum_compensatory_reg  = rowSums(across(c(purging_monthly, fasting_monthly, reg_exercise_14_16)))) |>
+  mutate(reg_compensatory = case_when(sum_compensatory_reg>0 ~1,
+                                      sum_compensatory_reg==0 ~0))
+
+
+#sets 'no low weight' and 'no high weight' as the reference categories for their 
+df_16 <- within(df_16, low_wt_category <- relevel(low_wt_category, ref = '0.No Low Wt'))
+df_16 <- within(df_16, high_wt_category <- relevel(high_wt_category, ref = '0.No High Wt'))
+
+
+
+bmi_13_16 <- bmi_13_16 |>  group_by(id, .model) |> 
+  summarise(bmi_loss_99 = case_when (sum(lower_99_cutoff, na.rm =TRUE) >0 ~1,
                                     sum(lower_99_cutoff, na.rm = TRUE)==0 ~0),
             bmi_loss_95 =  case_when(sum(lower_99_cutoff, na.rm =TRUE)>0 ~1,
                                      sum(lower_99_cutoff, na.rm = TRUE)==0 ~0),
@@ -61,13 +92,39 @@ bmi_13_16 <- bmi_df_13_16 |>  group_by(id) |>
                                      sum(upper_95_cutoff, na.rm = TRUE)==0 ~0),
             ow_cutoff =  case_when(sum(overweight_cutoff, na.rm =TRUE)>0 ~1,
                                    sum(overweight_cutoff, na.rm = TRUE)==0 ~0)) |>
-  mutate(low_wt_category = factor(case_when(bmi_loss_99 == 0 & thin_cutoff == 0 ~ 'No Low Wt',
-                                            bmi_loss_99 == 0 & thin_cutoff == 1 ~ 'UW - No BMIZ Reduce',
-                                            bmi_loss_99 == 1 & thin_cutoff == 0 ~ 'BMIZ Reduced - No UW',
-                                            bmi_loss_99 == 1 & thin_cutoff == 1 ~ 'UW and BMIZ Reduced'))) |>
-  mutate(high_wt_category = factor(case_when(bmi_gain_99 == 0 & ow_cutoff == 0 ~ 'No High Wt',
-                                             bmi_gain_99 == 0 & ow_cutoff == 1 ~ 'OW - No BMIZ Elevate',
-                                             bmi_gain_99 == 1 & ow_cutoff == 0 ~ 'BMIZ Elevated - no OW',
-                                             bmi_gain_99 == 1 & ow_cutoff == 1 ~ 'OW plus BMIZ Elevate')))
+  mutate(low_wt_category = factor(case_when(bmi_loss_99 == 0 & thin_cutoff == 0 ~ '0.No Low Wt',
+                                            bmi_loss_99 == 0 & thin_cutoff == 1 ~ '1.UW - No BMIZ Reduce',
+                                            bmi_loss_99 == 1 & thin_cutoff == 0 ~ '2.BMIZ Reduced - No UW',
+                                            bmi_loss_99 == 1 & thin_cutoff == 1 ~ '3.UW and BMIZ Reduced'))) |>
+  mutate(high_wt_category = factor(case_when(bmi_gain_99 == 0 & ow_cutoff == 0 ~ '0.No High Wt',
+                                             bmi_gain_99 == 0 & ow_cutoff == 1 ~ '1.OW - No BMIZ Elevate',
+                                             bmi_gain_99 == 1 & ow_cutoff == 0 ~ '2.BMIZ Elevated - no OW',
+                                             bmi_gain_99 == 1 & ow_cutoff == 1 ~ '3.OW plus BMIZ Elevate')))
 
 
+load('data/forecast_data.RData')
+
+bmi_pred <- left_join(bmi_13_16, sex_vars)  #joins sex variables with the bmi predictions at age 13-16
+
+bmi_pred_Mean <- bmi_pred |> 
+  filter(.model == 'Mean')
+bmi_pred_Mean.f <- bmi_pred_Mean %>% 
+  filter(sex_z == 2)
+bmi_pred_Mean.m <- bmi_pred_Mean %>% 
+  filter(sex_z == 1)
+bmi_pred_RW <- bmi_pred |> 
+  filter(.model == 'RW')
+bmi_pred_RW.f <- bmi_pred_RW %>% 
+  filter(sex_z == 2)
+bmi_pred_RW.m <- bmi_pred_RW %>% 
+  filter(sex_z == 1)
+bmi_pred_RWM <- bmi_pred |> 
+  filter(.model == 'M1')
+bmi_pred_RWM.f <- bmi_pred_M1 %>% 
+  filter(sex_z == 2)
+bmi_pred_RWM.m <- bmi_pred_M1 %>% 
+  filter(sex_z == 1)
+
+library(cgwtools)
+
+save(bmi_13_16, df_16, bmi_pred, bmi_pred_Mean, bmi_pred_Mean.f, bmi_pred_Mean.m, bmi_pred_RW, bmi_pred_RW.f, bmi_pred_RW.m, bmi_pred_M1, bmi_pred_M1.f, bmi_pred_M1.m, file = 'data/13_16.RData')
--- R/tabs-and-graphs.R
+++ R/tabs-and-graphs.R
@@ -37,25 +37,25 @@ frq_table_by_wt_sex_high <- function(df, x_var) {
   
 }
 
-
-
-#Binge eating table
-
-binge_table <- frq_table_by_wt_sex_high(df_14, binge_freq_d) |>
-  filter(binge_freq_d == '1x/mo or more; present') |>
-  mutate (sex_z = recode(sex_z, '1' = 'Boys', '2' = 'Girls'))
-
-
-
-
-ggplot(data = binge_table,
-       aes(x = high_wt_category, y = pct, fill = high_wt_category, label = sprintf('%0.1f%%', pct*100))) + #set the aesthetic features, including the labels at one decimal place
-  geom_bar(position = position_stack(reverse = TRUE), stat = 'identity') + #add column graph, with male and female separated
-  scale_y_continuous(labels = scales::percent, limits = c(0,0.2))+ #set the lower and upper limits of the y axis ( 0-100 percent)
-  theme(legend.title = element_blank(), axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), text = element_text(size = 15))+ #make legend text a bit bigger
-  labs(x = element_blank(), y = 'Percent', title = 'Binge Eating Across Weight Elevation Groups') +
-  geom_text(aes(label = sprintf('%0.1f%%', pct*100)), position = position_stack(reverse = TRUE, vjust = 0.5), color = 'white', size = 4, fontface = 'bold') +
-  facet_wrap(~sex_z) +
-  theme_classic()+
-  theme(legend.position = 'none', axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), text = element_text(size = 15)) + #make legend text a bit bigger
-  scale_fill_manual(values = wes_palette(name = 'Rushmore', n = 4))
+# 
+# 
+# #Binge eating table
+# 
+# binge_table <- frq_table_by_wt_sex_high(df_14, binge_freq_d) |>
+#   filter(binge_freq_d == '1x/mo or more; present') |>
+#   mutate (sex_z = recode(sex_z, '1' = 'Boys', '2' = 'Girls'))
+# 
+# 
+# 
+# 
+# ggplot(data = binge_table,
+#        aes(x = high_wt_category, y = pct, fill = high_wt_category, label = sprintf('%0.1f%%', pct*100))) + #set the aesthetic features, including the labels at one decimal place
+#   geom_bar(position = position_stack(reverse = TRUE), stat = 'identity') + #add column graph, with male and female separated
+#   scale_y_continuous(labels = scales::percent, limits = c(0,0.2))+ #set the lower and upper limits of the y axis ( 0-100 percent)
+#   theme(legend.title = element_blank(), axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), text = element_text(size = 15))+ #make legend text a bit bigger
+#   labs(x = element_blank(), y = 'Percent', title = 'Binge Eating Across Weight Elevation Groups') +
+#   geom_text(aes(label = sprintf('%0.1f%%', pct*100)), position = position_stack(reverse = TRUE, vjust = 0.5), color = 'white', size = 4, fontface = 'bold') +
+#   facet_wrap(~sex_z) +
+#   theme_classic()+
+#   theme(legend.position = 'none', axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), text = element_text(size = 15)) + #make legend text a bit bigger
+#   scale_fill_manual(values = wes_palette(name = 'Rushmore', n = 4))
